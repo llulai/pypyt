044498e57d70a6a9f4164f7d1d4a8188f528ac18
Julio Gajardo <j.gajardo@criteo.com>
2018-05-02 14:44:32 -0300

    updated file dir

--- tests/test_pypyt.py
+++ tests/test_pypyt.py
@@ -3,4 +3,4 @@ from pypyt import open_ppt
 
 
 def test_open():
-    assert isinstance(open_ppt('tests\__template__.pptx'), Presentation)
+    assert isinstance(open_ppt('tests/__template__.pptx'), Presentation)
