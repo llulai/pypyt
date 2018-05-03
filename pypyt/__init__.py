fbb886a94cc0ac69229d63634791757463e61885
Julio Gajardo <j.gajardo@criteo.com>
2018-05-03 16:36:28 -0300

    updated docstrings

--- pypyt/__init__.py
+++ pypyt/__init__.py
@@ -4,23 +4,61 @@ import re
 from functools import singledispatch
 from pptx import Presentation
 from pptx.chart.chart import Chart
-from pptx.chart.data import ChartData
+from pptx.chart.data import CategoryChartData
 from pptx.shapes.base import BaseShape
 from pptx.shapes.table import Table
 from pptx.text.text import TextFrame
 from pandas import DataFrame
+from typing import Union
 
 
 # BASE
 def open_ppt(filename: str) -> Presentation:
-    """Opens a pptx file given the filename and returns it"""
+    """
+    Opens a pptx file given the filename and returns it.
+
+    Parameters
+    ----------
+    filename: str
+        The name of the file to be open.
+
+    Returns
+    -------
+    pptx.presentation.Presentation
+        Presentation object
+
+    Examples
+    --------
+    >>> open_ppt('template.pptx')
+    <pptx.presentation.Presentation at ...>
+    """
     return Presentation(filename)
 
 
-def render_ppt(template_name: str, values: dict) -> Presentation:
-    """Returns a rendered presentation given the template name and values to be rendered."""
-    # opens the presentation
-    prs = open_ppt(template_name)
+def render_ppt(prs: Presentation, values: dict) -> Presentation:
+    """
+    Returns a rendered presentation given the template name and values to be rendered.
+
+    Parameters
+    ----------
+    prs: pptx.presentation.Presentation
+        Presentation to be rendered.
+
+    values: dict
+        Dictionary with the values to render on the template.
+
+    Returns
+    -------
+    pptx.presentation.Presentation
+        Rendered presentation
+
+    Examples
+    --------
+    >>> prs = open_ppt('template.pptx')
+    >>> values = {'presentation_title': "My Cool Presentation"}
+    >>> render_ppt(prs, values)
+    <pptx.presentation.Presentation at ...>
+    """
 
     # checks each given item
     for key, value in values.items():
@@ -39,29 +77,148 @@ def render_ppt(template_name: str, values: dict) -> Presentation:
     return prs
 
 
+def render_template(template_name: str, values: dict) -> Presentation:
+    """
+    Returns a rendered presentation given the template name and values to be rendered.
+
+    Parameters
+    ----------
+    template_name: str
+        Name of the presentation to be rendered.
+
+    values: dict
+        Dictionary with the values to render on the template.
+
+    Returns
+    -------
+    pptx.presentation.Presentation
+        Rendered presentation
+
+    Examples
+    --------
+    >>> values = {'presentation_title': "My Cool Presentation"}
+    >>> render_template('template.pptx', values)
+    <pptx.presentation.Presentation at ...>
+    """
+    return render_ppt(open_ppt(template_name), values)
+
+
 def save_ppt(prs: Presentation, filename: str) -> None:
-    """Saves the given presentation with the given filename"""
+    """
+    Saves the given presentation with the given filename.
+
+    Parameters
+    ----------
+    prs: pptx.presentation.Presentation
+        Presentation to be saved.
+
+    filename: str
+        Name of the file to be saved.
+
+    Examples
+    --------
+    >>> values = {'presentation_title': "My Cool Presentation"}
+    >>> rendered_prs = render_template('template.pptx', values)
+    >>> save_ppt(rendered_prs, 'presentation.pptx')
+    """
     prs.save(filename)
 
 
 def render_and_save_ppt(template_name: str, values: dict, filename: str) -> None:
-    """Renders and save a presentation with the given template name,
-    values to be rendered, and filename."""
-    save_ppt(render_ppt(template_name, values), filename)
+    """
+    Renders and save a presentation with the given template name,
+    values to be rendered, and filename.
+    Parameters
+    ----------
+    template_name: str
+        Name of the presentation to be saved.
+
+    values: dict
+        Dictionary with the values to render on the template.
+
+    filename: str
+        Name of the file to be saved.
+
+    Examples
+    --------
+    >>> values = {'presentation_title': "My Cool Presentation"}
+    >>> render_and_save_ppt('template.pptx', values, 'presentation.pptx')
+    """
+    save_ppt(render_template(template_name, values), filename)
 
 
 def get_shapes_by_name(prs: Presentation, name: str) -> list:
-    """Returns a list of shapes with the given name in the given presentation"""
+    """
+    Returns a list of shapes with the given name in the given presentation.
+    Parameters
+    ----------
+    prs: pptx.presentation.Presentation
+        Presentation to be saved.
+
+    name: str
+        Name of the shape(s) to be returned.
+
+    Examples
+    --------
+    >>> prs = open_ppt('template.pptx')
+    >>> get_shapes_by_name(prs, 'chart')
+    [<pptx.shapes.placeholder.PlaceholderGraphicFrame at ...>]
+
+    """
     return [shape for slide in prs.slides for shape in slide.shapes if shape.name == name]
 
 
-def get_shapes(prs: Presentation) -> dict:
-    """Returns a dictionary with the shape name as keys and shape type as values"""
-    return {shape.name: get_shape_type(shape) for slide in prs.slides for shape in slide.shapes}
+def get_shapes(prs: Presentation) -> tuple:
+    """
+    Returns a tuple of tuples with the shape name and shape type.
+
+    Parameters
+    ----------
+    prs: pptx.presentation.Presentation
+        Presentation to get the shapes from.
+
+    Returns
+    -------
+    tuple:
+        Tuple with the shapes.
+
+    Examples
+    --------
+    >>> prs = open_ppt('template.pptx')
+    >>> get_shapes(prs)
+    (('client_name', 'paragraph'),
+     ('presentation_title', 'paragraph'),
+     ('slide_text', 'paragraph'),
+     ('slide_title', 'paragraph'),
+     ('chart', 'chart'),
+     ('Title 1', 'paragraph'),
+     ('table', 'table'),
+     ('Title 1', 'paragraph'))
+    """
+    return tuple((shape.name, get_shape_type(shape)) for slide in prs.slides for shape in slide.shapes)
 
 
 def get_shape_type(shape: BaseShape) -> str:
-    """Returns a string with the kind of the given shape."""
+    """
+    Returns a string with the kind of the given shape.
+
+    Parameters
+    ----------
+    shape: BaseShape
+        Shape to get the type from.
+
+    Returns
+    -------
+    string:
+        String representing the type of the shape
+
+    Examples
+    --------
+    >>> prs = open_ppt('template.pptx')
+    >>> shapes = get_shapes_by_name(prs, 'client_name')
+    >>> get_shape_type(shapes[0])
+    'paragraph'
+    """
     if is_paragraph(shape):
         return 'paragraph'
     elif is_table(shape):
@@ -88,8 +245,18 @@ def is_chart(shape: BaseShape) -> bool:
 
 # RENDER TABLE
 @singledispatch
-def render_table(_, __) -> None:
-    """Renders a table with the given values"""
+def render_table(values: Union[dict, list, DataFrame], table: Table) -> None:  # pylint: disable=unused-argument
+    """
+    Renders a table with the given values.
+
+    Parameters
+    ----------
+    values: Union[dict, list, DataFrame]
+        Values to render the table
+
+    table: Table
+        Table object to be rendered.
+    """
     raise NotImplementedError
 
 
@@ -102,26 +269,50 @@ def _(values: dict, table: Table) -> None:
             render_paragraph(values, cell.text_frame)
 
 
+@render_table.register(DataFrame)
+def _(values: DataFrame, table: Table) -> None:
+    # TODO: raise error if size(values) != size(table)
+    table_rows = iter(table.rows)
+
+    if hasattr(values, 'header') and values.header:
+        for values_cell, table_cell in zip(list(values), next(table_rows)):
+            render_paragraph(values_cell, table_cell.text_frame)
+
+    for values_row, table_row in zip(list(values.values), table_rows):
+        for values_cell, table_cell in zip(list(values_row), table_row.cells):
+            render_paragraph(values_cell, table_cell.text_frame)
+
+
 @render_table.register(list)
 def _(values: list, table: Table) -> None:
     """In the case you want to replace the whole table,
     it will set the value for each cell in the list"""
-    for i, row in enumerate(table.rows):
-        for j, cell in enumerate(row.cells):
-            render_paragraph(values[i][j], cell.text_frame)
+    # TODO: raise error if size(values) != size(table)
+    for values_row, table_row in zip(values, table.rows):
+        for values_cell, table_cell in zip(values_row, table_row.cells):
+            render_paragraph(values_cell, table_cell.text_frame)
 
 
 # RENDER PARAGRAPH
 @singledispatch
 def render_paragraph(values, text_frame: TextFrame) -> None:
-    """In the case you want to replace the whole text"""
+    """
+    In the case you want to replace the whole text.
+
+    Parameters
+    ----------
+    values: Union[dict, str, int, float]
+        Values to render the text.
+
+    text_frame: TextFrame
+        TextFrame object to be rendered.
+    """
     paragraph = text_frame.paragraphs[0]
     p = paragraph._p  # pylint: disable=protected-access,invalid-name
-    if p:
-        for idx, run in enumerate(paragraph.runs):
-            if idx == 0:
-                continue
-            p.remove(run._r)  # pylint: disable=protected-access
+    for idx, run in enumerate(paragraph.runs):
+        if idx == 0:
+            continue
+        p.remove(run._r)  # pylint: disable=protected-access
     else:
         paragraph.text = 't'
     paragraph.runs[0].text = values
@@ -145,14 +336,26 @@ def _(values: dict, text_frame: TextFrame) -> None:
 
 # RENDER GRAPH
 @singledispatch
-def render_chart(values, _, chart_data=ChartData()) -> None:
-    """Renders into the given chart the given values."""
+def render_chart(values: Union[dict, DataFrame], chart: Chart) -> None:  # pylint: disable=unused-argument
+    """
+    Renders the given values into the given chart.
+
+    Parameters
+    ----------
+    values: Union[dict, DataFrame]
+        Values to render the chart.
+
+    chart: Chart
+        Chart object to be rendered.
+    """
     raise NotImplementedError(f"Method not implemented for {type(values)} object type")
 
 
 @render_chart.register(dict)
-def _(values: DataFrame, chart: Chart, chart_data=ChartData()) -> None:
+def _(values: DataFrame, chart: Chart) -> None:
     """Renders into the given chart the values in the DataFrame."""
+
+    chart_data = CategoryChartData()
 
     chart_data.categories = values['categories']
 
@@ -166,8 +369,10 @@ def _(values: DataFrame, chart: Chart, chart_data=ChartData()) -> None:
 
 
 @render_chart.register(DataFrame)
-def _(values: DataFrame, chart: Chart, chart_data=ChartData()) -> None:
+def _(values: DataFrame, chart: Chart) -> None:
     """Renders into the given chart the values in the DataFrame."""
+
+    chart_data = CategoryChartData()
 
     chart_data.categories = list(values.index)
 
--- tests/test_pypyt.py
+++ tests/test_pypyt.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from pytest import fixture
+from pytest import fixture, raises
 
 from pptx.presentation import Presentation
 from pypyt import open_ppt, get_shapes_by_name, render_chart
@@ -12,17 +12,21 @@ FakeShape = namedtuple('Shape', 'name')
 
 class Dummy:
     text_frame = None
+    categories = None
+    series = None
+
 
 
 class FakeChart:
     def __init__(self):
-        self.chart_data = None
+        self.chart_data = Dummy()
         self.chart_title = Dummy()
         self.chart_title.text_frame = Dummy()
         self.chart_title.text_frame.text = None
 
     def __eq__(self, other):
-        return (self.chart_data == other.chart_data
+        return (self.chart_data.categories == other.chart_data.categories
+                and self.chart_data.series == other.chart_data.series
                 and self.chart_title.text_frame.text == other.chart_title.text_frame.text)
 
     def __repr__(self):
@@ -31,20 +35,8 @@ class FakeChart:
                f" title: {self.chart_title.text_frame.text}}}"
 
     def replace_data(self, chart_data):
-        self.chart_data = chart_data
-
-
-class FakeChartData:
-    def __init__(self):
-        self.categories = None
-        self.series = []
-
-    def __eq__(self, other):
-        return (self.categories == other.categories
-                and sorted(self.series) == sorted(other.series))
-
-    def add_series(self, label: str, series: list):
-        self.series.append((label, series))
+        self.chart_data.categories = [cat.label for cat in chart_data.categories]
+        self.chart_data.series = {s.name: s.values for s in chart_data._series}
 
 
 @fixture
@@ -100,8 +92,8 @@ def test_chart_same_output_no_title(chart_values_dict, chart_values_df):
     fake_chart_dict = FakeChart()
     fake_chart_df = FakeChart()
 
-    render_chart(chart_values_df, fake_chart_dict, chart_data=FakeChartData())
-    render_chart(chart_values_dict, fake_chart_df, chart_data=FakeChartData())
+    render_chart(chart_values_df, fake_chart_dict)
+    render_chart(chart_values_dict, fake_chart_df)
 
     assert fake_chart_dict == fake_chart_df
 
@@ -114,7 +106,14 @@ def test_chart_same_output_title(chart_values_dict, chart_values_df):
     fake_chart_dict = FakeChart()
     fake_chart_df = FakeChart()
 
-    render_chart(chart_values_df, fake_chart_dict, chart_data=FakeChartData())
-    render_chart(chart_values_dict, fake_chart_df, chart_data=FakeChartData())
+    render_chart(chart_values_df, fake_chart_dict)
+    render_chart(chart_values_dict, fake_chart_df)
 
     assert fake_chart_dict == fake_chart_df
+
+
+def test_chart_invalid_values():
+    chart_invalid_data  = [1, 2, 3]
+
+    with raises(NotImplementedError):
+        render_chart(chart_invalid_data, FakeChart())
