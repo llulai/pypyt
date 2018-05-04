3e78c914d5d1a4cf7e80a5ca2028d08baf505be7
Julio Gajardo <j.gajardo@criteo.com>
2018-05-04 13:49:58 -0300

    updated folders to be downloaded

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
     packages=['pypyt'],
     install_requires=['python-pptx'],
     package_data={
-        'pypyt': ['html_docs/*', 'html_docs/_images/*', 'html_docs/_sources/*', 'html_docs/_static/*', 'html_docs/source/*']
+        'pypyt': ['html_docs/*', 'html_docs/_images/*', 'html_docs/_sources/*', 'html_docs/_static/*',
+                  'html_docs/source/*', 'html_docs/_static/css/*', 'html_docs/_static/fonts/*',
+                  'html_docs/_static/js/*']
     }
 )
 
