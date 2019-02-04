77fd7bd20a359aa7693106c9391308aa257bb7d0
Julio Gajardo <j.gajardo@criteo.com>
2019-02-04 16:13:39 -0200

    removed numpy from requirements

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Julio Gajardo'
 author = 'Julio Gajardo'
 
 # The short X.Y version
-version = '0.0.22'
+version = '0.0.23'
 # The full version, including alpha/beta/rc tags
-release = '0.0.22'
+release = '0.0.23'
 
 
 # -- General configuration ---------------------------------------------------
