bb8f6a76635b9979a7ef21b311cb77a66f9f31c1
Julio Gajardo <j.gajardo@criteo.com>
2019-06-11 17:33:49 -0300

    updated pandas version

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Julio Gajardo'
 author = 'Julio Gajardo'
 
 # The short X.Y version
-version = '0.0.26'
+version = '0.0.27'
 # The full version, including alpha/beta/rc tags
-release = '0.0.26'
+release = '0.0.27'
 
 
 # -- General configuration ---------------------------------------------------
