92dd71f73a463561e1aebe82cc03e7e60f69fc7e
Julio Gajardo <j.gajardo@criteo.com>
2019-07-24 13:51:46 -0300

    updated version

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Julio Gajardo'
 author = 'Julio Gajardo'
 
 # The short X.Y version
-version = '0.0.31'
+version = '0.0.32'
 # The full version, including alpha/beta/rc tags
-release = '0.0.31'
+release = '0.0.32'
 
 
 # -- General configuration ---------------------------------------------------
